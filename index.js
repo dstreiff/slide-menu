a85a1f13d4f58c85226b6ff2eec3e5ef7ad73c79
dstreiff <dstreiff@gmx.ch>
2022-01-16 23:24:50 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('sdfad');
+
+const marker = document.querySelector('#marker');
+const items = document.querySelectorAll('nav a');
+
+ 
+function eventFunction(e){
+    marker.style.left = e.target.offsetLeft+'px';
+    marker.style.width = e.target.offsetWidth+'px';
+}
+items.forEach(link => {
+    link.addEventListener('click',eventFunction);
+});
